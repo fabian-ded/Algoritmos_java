b7e2e15f073c959622e28e9e993e03cf05b02f50
Fabb casa <Fabiansil902@gmail.com>
2026-08-07 18:35:43 -0500

    actualizacion

--- Algoritmo_nine.java
+++ Algoritmo_nine.java
@@ -1,3 +1,8 @@
 /* Calcular el salario neto de un trabajador en función del número de horas trabajadas, el precio de la
 hora y considerando un descuento fijo al sueldo base por concepto de impuestos del 20%. */
 
+public class Algoritmo_nine {
+    public static void main(String[] args) {
+        
+    }
+}
