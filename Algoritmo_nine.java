f0c094d757d0099616b9afcb4f79edf35b927158
Fabb casa <Fabiansil902@gmail.com>
2026-08-07 19:47:46 -0500

    Finalizacion hasta el ejercicios 10

--- Algoritmo_nine.java
+++ Algoritmo_nine.java
@@ -3,6 +3,19 @@ hora y considerando un descuento fijo al sueldo base por concepto de impuestos d
 
 public class Algoritmo_nine {
     public static void main(String[] args) {
-        
+        int horas;
+        double precio_horas;
+
+        System.out.println("Cuantas horas trabajo?: ");
+        horas = Integer.parseInt(System.console().readLine());
+        System.out.println("Cuantas horas trabajo?: ");
+        precio_horas = Double.parseDouble(System.console().readLine());
+
+        double valor_trabajado = horas*precio_horas;
+        double descuento_total = valor_trabajado*0.20;
+
+        System.out.println("Precio total de horas trabajadas: " + valor_trabajado );
+        System.out.println("Valor final a pagar con impuestos: " + descuento_total);
+
     }
 }
--- Algoritmo_ten.java
+++ Algoritmo_ten.java
@@ -6,6 +6,33 @@
 1 franco francés = 20.110 pesetas
 1 libra esterlina = 178.938 pesetas
 100 liras italianas = 9.289 pesetas
-Lea una cantidad en chelines austriacos e imprima el equivalente en pesetas. Lea una
-cantidad en dracmas griegos e imprima su equivalente en francos franceses. Finalmente, lea una
-cantidad en pesetas e imprima su equivalente en dólares y liras italianas. */
+Lea una cantidad en chelines austriacos e imprima el equivalente en pesetas. ya
+Lea una cantidad en dracmas griegos e imprima su equivalente en francos franceses.  
+Finalmente, lea una cantidad en pesetas e imprima su equivalente en dólares y liras italianas. */
+
+public class Algoritmo_ten {
+    public static void main(String[] args) {
+        
+        double moneda_aus;
+        double moneda_drag;
+        double pesetas;
+
+        System.out.println("Monedas en chelines austriacos");
+        moneda_aus = Double.parseDouble(System.console().readLine());
+        double valor_drag = (moneda_aus*956.871)/100;
+        System.out.println("Moneda de chelines autriacos a pesetas es de: " + valor_drag);
+
+        System.out.println("Monedas en dragma");
+        moneda_drag = Double.parseDouble(System.console().readLine());
+        double valor_en_pesetas = (moneda_drag*88.607)/100;
+        double valor_fran = valor_en_pesetas/20.110;
+        System.out.println("Moneda de dracmas griegos a francos franceses es de: " + valor_fran);
+
+        System.out.println("Monedas en pesetas");
+        pesetas = Double.parseDouble(System.console().readLine());
+        double valor_dol = pesetas/122.499;
+        double valor_liras = (pesetas*9.289)/100;
+        System.out.println("Moneda de pesetas a dólar EEUU es de: " + valor_dol);
+        System.out.println("Moneda de pesetas a liras italianas es de: " + valor_liras);
+    }
+}
